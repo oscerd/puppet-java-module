00b204fcafbe06b208e74ff89370c416c0fffc21
ancosen <ancosen@gmail.com>
2014-07-26 21:20:59 +0200

    Indentation changes

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -64,7 +64,7 @@ define java::setup (
       alias => java_home
   }
   
-    exec { 'move_java': 
+  exec { 'move_java': 
           command => "mv ${defined_tmpdir}${type}1.${family}.0_${update_version}/ ${java_home_base}",
           require => [ File[ java_home ], 
                        Exec[ extract ] ] }
