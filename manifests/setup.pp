00c08aa2447c45aa250b51abd97704a4fe5d04a4
ancosen <ancosen@gmail.com>
2014-07-27 11:24:22 +0200

    Added java, javac, javaws install and setup phase

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -43,13 +43,12 @@ define java::setup (
   }
   
   package { 'tar':
-      ensure => installed,
-      alias => tar   
-  }
+			      ensure => installed,
+			      alias => tar   }
   
   file { "${defined_tmpdir}${type}-${family}u${update_version}-${os}-${architecture}${extension}":
-      ensure => present,
-      source => "puppet:///modules/java/${type}-${family}u${update_version}-${os}-${architecture}${extension}" }
+		      ensure => present,
+		      source => "puppet:///modules/java/${type}-${family}u${update_version}-${os}-${architecture}${extension}" }
 
   exec { 'extract_java': 
           command => "tar -xzvf ${defined_tmpdir}${type}-${family}u${update_version}-${os}-${architecture}${extension} -C ${defined_tmpdir}",
@@ -58,15 +57,43 @@ define java::setup (
           alias => extract }
                        
   file { "$java_home_base":
-      ensure => directory,
-      mode => '755',
-      owner => 'root', 
-      alias => java_home
-  }
+		      ensure => directory,
+		      mode => '755',
+		      owner => 'root', 
+		      alias => java_home }
   
   exec { 'move_java': 
           command => "mv ${defined_tmpdir}${type}1.${family}.0_${update_version}/ ${java_home_base}",
           require => [ File[ java_home ], 
                        Exec[ extract ] ] }
-  
+                       
+  exec { 'install_java':
+          require => Exec ['move_java'],
+          logoutput => true,
+          command => "update-alternatives --install /bin/java java ${java_home_base}/${type}1.${family}.0_${update_version}/bin/java 1"  }
+
+  exec { 'set_java':
+          require => Exec ['install_java'],
+          logoutput => true,
+          command => "update-alternatives --set java ${java_home_base}/${type}1.${family}.0_${update_version}/bin/java"  } 
+          
+  exec { 'install_javac':
+          require => Exec ['move_java'],
+          logoutput => true,
+          command => "update-alternatives --install /bin/javac javac ${java_home_base}/${type}1.${family}.0_${update_version}/bin/javac 1"  }
+
+  exec { 'set_javac':
+          require => Exec ['install_javac'],
+          logoutput => true,
+          command => "update-alternatives --set javac ${java_home_base}/${type}1.${family}.0_${update_version}/bin/javac"  } 
+          
+  exec { 'install_javaws':
+          require => Exec ['move_java'],
+          logoutput => true,
+          command => "update-alternatives --install /bin/javaws javaws ${java_home_base}/${type}1.${family}.0_${update_version}/bin/javaws 1"  }
+
+  exec { 'set_javaws':
+          require => Exec ['install_javaws'],
+          logoutput => true,
+          command => "update-alternatives --set javaws ${java_home_base}/${type}1.${family}.0_${update_version}/bin/javaws"  } 
   }
