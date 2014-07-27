b2bbdf44b0d9d0cf4ca049fb843953db296381aa
ancosen <ancosen@gmail.com>
2014-07-27 15:06:47 +0200

    Parameters validation improvement. New usage example in README.md

--- README.md
+++ README.md
@@ -20,6 +20,26 @@ Clone this repository in a java directory in your puppet module directory
 
 ## <a name='Usage'>Usage</a>
 
+If you include the java::setup class the module will take the package from '/java/files' folder, extract his content it and move it 
+in a specific directory (based on the OS we are working on). Here is an example:
+
+	java::setup { "java":
+	  type => "jdk",
+	  family => "7",
+	  update_version => "65",
+	  architecture => "x64",
+	  os => "linux",
+	  extension => ".tar.gz",
+	  tmpdir => ""
+	  }
+
+It's important to define a global search path for the `exec` resource to make module work. 
+This should usually be placed in `manifests/site.pp`:
+
+	Exec {
+	  path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+	}
+
 ## <a name='Contributing'>Contributing</a>
 
 Feel free to contribute by testing, opening issues and adding/changing code
--- manifests/setup.pp
+++ manifests/setup.pp
@@ -29,10 +29,8 @@ define java::setup (
     fail('Operating system parameter must be set')
   }
   
-  $java_home_base = $operatingsystem ? {
-    /(?i:SLES|OpenSuSE)/ => '/usr/lib/java',
-    /(?i:Ubuntu|Debian|Mint|Centos)/ => '/usr/lib/jvm',
-    default => '/usr/lib/jvm',
+  if ($extension == undef) {
+    fail('Extension parameter must be set')
   }
   
   if ($tmpdir == undef){
@@ -40,6 +38,34 @@ define java::setup (
     $defined_tmpdir ='/tmp/'
   } else {
     $defined_tmpdir = $tmpdir
+  }
+  
+  # Validate parameters  
+  if (($type != 'jdk') and ($type != 'jre')) {
+    fail('type parameter must be "jdk" or "jre"')
+  }
+  
+  if (($family != '6') and ($family != '7') and ($family != '8')) {
+    fail('family parameter must be between "6" and "8" included')
+  }
+  
+  if (($architecture != "i586") and ($architecture != "x64") and ($architecture != "sparc") and ($architecture != "sparcv9")) {
+    fail('architecture parameter must be "i586","x64","sparc","sparcv9"')
+  }
+
+  if (($os != "linux") and ($os != "solaris")) {
+    fail('Operating system parameter must "linux" or "solaris"')
+  }
+
+  if ($extension != ".tar.gz") {
+    fail('Extension parameter must ".tar.gz')
+  }
+   
+  $java_home_base = $operatingsystem ? {
+    /(?i:SLES|OpenSuSE)/ => '/usr/lib/java',
+    /(?i:Ubuntu|Debian|Mint|Centos)/ => '/usr/lib/jvm',
+    /(?i:Solaris)/ => '/developer/java',   
+    default => '/usr/lib/jvm',
   }
   
   package { 'tar':
@@ -96,4 +122,8 @@ define java::setup (
           require => Exec ['install_javaws'],
           logoutput => true,
           command => "update-alternatives --set javaws ${java_home_base}/${type}1.${family}.0_${update_version}/bin/javaws"  } 
+          
+  file { "/etc/profile.d/java.sh":
+          content => "export JAVA_HOME=${java_home_base}/${type}1.${family}.0_${update_version}/
+                      export PATH=\$PATH:\$JAVA_HOME/bin"  }
   }
